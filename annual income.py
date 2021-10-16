b5bf126fe0d1559549c49ad51bdf34a58f42e7de
vpriyanka1819 <92620444+vpriyanka1819@users.noreply.github.com>
2021-10-16 11:38:04 +0530

    Add files via upload

---
+++ annual income.py
@@ -0,0 +1,12 @@
+'''
+Find the income tax that the user should pay for a year.
+'''
+
+Annual_income = float(input("enter the annual income:"))
+#if (Annual_income <= 100000)
+tax = 10 * (Annual_income / 100) 
+print("Annual_income for the year is:", tax )
+ 
+
+
+
